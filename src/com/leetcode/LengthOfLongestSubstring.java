f40ee36d0356781e6b7d89899755143abd7fe4fa
Umesh Tiwari <umesh.u.tiwari@gmail.com>
2026-01-23 02:01:04 +0530

    adding the LengthOfLongestSubstring program

---
+++ src/com/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LengthOfLongestSubstring {
+
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+
+            // Remove characters until duplicate is removed
+            while (set.contains(currentChar)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(currentChar);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        // Test cases
+        String s1 = "abcabcbb";
+        String s2 = "bbbbb";
+        String s3 = "pwwkew";
+        String s4 = "";
+
+        System.out.println("Input: " + s1);
+        System.out.println("Output: " + lengthOfLongestSubstring(s1));
+
+        System.out.println("\nInput: " + s2);
+        System.out.println("Output: " + lengthOfLongestSubstring(s2));
+
+        System.out.println("\nInput: " + s3);
+        System.out.println("Output: " + lengthOfLongestSubstring(s3));
+
+        System.out.println("\nInput: \"" + s4 + "\"");
+        System.out.println("Output: " + lengthOfLongestSubstring(s4));
+    }
+}
